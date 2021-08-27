62c82653565602b0bee7eb8ab5f0eb923f0dded2
Miguel Cobas <cobas.miguel@gmail.com>
2021-08-27 01:47:39 -0500

    Se cambio de available por status

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -91,12 +91,12 @@ const Products = () => {
                                     <td className="align-middle">{product.code}</td>
                                     <td className="align-middle">{product.category.name}</td>
                                     <td className="align-middle">{product.client.company_name}</td>
-                                    <td className="align-middle text-center">{product.available ? <FiberManualRecordIcon style={{ color: green[500] }} /> : <FiberManualRecordIcon color="secondary" />}</td>
+                                    <td className="align-middle text-center">{product.status ? <FiberManualRecordIcon style={{ color: green[500] }} /> : <FiberManualRecordIcon color="secondary" />}</td>
                                     <td className="align-middle text-right">
                                         {/* <IconButton aria-label="editar" color="primary" onClick={() => edit(product)}>
                                             <EditIcon />
                                         </IconButton> */}
-                                        {product.available && user.role === 'ADMIN' || user.role === 'SUPER_ADMIN' ?
+                                        {product.status && user.role === 'ADMIN' || user.role === 'SUPER_ADMIN' ?
                                             <IconButton aria-label="delete" color="secondary" onClick={() => removeProduct(product._id)}>
                                                 <DeleteIcon />
                                             </IconButton> : ''}
