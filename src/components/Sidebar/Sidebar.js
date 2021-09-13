f240da716f70afdfff87f4159ff9c16472504cc6
Miguel Cobas <cobas.miguel@gmail.com>
2021-09-13 16:39:33 -0500

    Se corrigio el logo si trae

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -51,16 +51,18 @@ const Sidebar = (props) => {
   return (
     <>
       <div className="bg-white h-full flex flex-col justify-start	border-r print:hidden" >
-      <div className={!collapse ? " flex flex-row items-center justify-center" : "flex flex-row items-center justify-center "}>
-          {company ?
-            company.image ? <img src={company.image} width='60px' height='auto' alt={company.name} />
-              : company.name ? <h5>{company.name}</h5> : null  : null}
+        <div className={!collapse ? "flex flex-row justify-between pl-5" : "flex flex-col"}>
+          <div className={!collapse ? "flex flex-row items-center justify-center" : "flex flex-row items-center justify-center "}>
+            {company ?
+              company.image ? <img src={company.image} width={collapse ? '60px' : '120px'} height='auto' alt={company.name} />
+                : company.name ? <h5>{company.name}</h5> : null : null}
           </div>
-        <div className={!collapse ? "py-2 flex flex-row items-center justify-end " : "py-2 flex flex-row items-center justify-center "}>
-          
-          <IconButton arial-label="menu" className="mx-4" onClick={menuCollapse}>
-            <MenuIcon />
-          </IconButton>
+          <div className={!collapse ? "py-2 flex flex-row items-center justify-end " : "py-2 flex flex-row items-center justify-center "}>
+
+            <IconButton arial-label="menu" className="mx-4" onClick={menuCollapse}>
+              <MenuIcon />
+            </IconButton>
+          </div>
         </div>
         <div className={!collapse ? "bg-gray-100 pt-1 px-2 pb-2 rounded-xl flex justify-center items-center mx-4" : "bg-gray-100 pt-1 px-2 pb-2 rounded-xl flex justify-center items-center mx-2 "}>
           {user.image ? <img className="rounded-full w-10 my-2 " src={user.image} alt="avatar" /> : <AccountCircleIcon fontSize='large' />}
@@ -68,7 +70,7 @@ const Sidebar = (props) => {
           {!collapse && <h2 className="text-lg mx-3  my-2 ">{user.name ? user.name : null}</h2>}
         </div>
         <nav className="flex flex-col pt-8 flex-1">
-          <NavLink to="/dashboard" className={!collapse ? "pl-7 py-3 hover:bg-blue-100 no-underline	text-gray-500 flex flex-row items-center justify-start":"pl-4 py-3 hover:bg-blue-100 no-underline	text-gray-500 flex flex-row items-center justify-start"} activeClassName="bg-blue-100 text-blue-500"> <DashboardRoundedIcon /> {!collapse && ' Dashboard'}</NavLink>
+          <NavLink to="/dashboard" className={!collapse ? "pl-7 py-3 hover:bg-blue-100 no-underline	text-gray-500 flex flex-row items-center justify-start" : "pl-4 py-3 hover:bg-blue-100 no-underline	text-gray-500 flex flex-row items-center justify-start"} activeClassName="bg-blue-100 text-blue-500"> <DashboardRoundedIcon /> {!collapse && ' Dashboard'}</NavLink>
           {user.role === 'SUPER_ADMIN' || user.role === 'SUPPORT' ?
             <NavLink to="/companies" className={!collapse ? "pl-7 py-3 hover:bg-blue-100 no-underline	text-gray-500 flex flex-row items-center justify-start" : "pl-4 py-3 hover:bg-blue-100 no-underline	text-gray-500 flex flex-row items-center justify-start"} activeClassName="bg-blue-100 text-blue-500"> <HomeWork /> {!collapse && ' Compañias'}</NavLink>
             : <></>}
