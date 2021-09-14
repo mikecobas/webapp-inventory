937f6ef326d98b52460094be5a63844388f95c60
Miguel Cobas <cobas.miguel@gmail.com>
2021-09-14 00:35:20 -0500

    Se mejoro el dashboard

--- src/pages/dashoard.js
+++ src/pages/dashoard.js
@@ -48,6 +48,7 @@ const Dashboard = () => {
     [
       ["Cliente", "Productos", { role: "style" }]
     ])
+  const [topProductInventory, setTopProductInventory] = useState(null)
   const [transactionList, setTransactionList] = useState(null)
   const [loading, setLoading] = useState(true)
 
@@ -85,7 +86,7 @@ const Dashboard = () => {
       const resTotalProducts = await Api.getDashboardProductsByClients()
       setDataPie([...dataPie, resTransaction.dataPie.enter, resTransaction.dataPie.exit])
       setDataProductsByClients(resTotalProducts.clients)
-
+      setTopProductInventory(resTotalProducts.productsByClients)
 
       console.log(dataProductsByClients)
       setTransactionList(resTransaction.transactionList)
@@ -96,8 +97,8 @@ const Dashboard = () => {
 
 
   return (
-    <div className="px-10 h-full overflow-auto bg-white dark:bg-red-400">
-      <div className="flex flex-col lg:flex-row justify-between  items-stretch sm:items-center print:items-start mb-1">
+    <div className="px-10 h-full overflow-auto">
+      <div className="flex flex-col lg:flex-row justify-between  items-stretch  print:items-start mb-1">
         <div className="mt-3">
           <h1 className="text-3xl font-bold">Dashboard </h1>
           <h3 className="font-normal text-lg">Hola, {user.name}</h3>
@@ -239,22 +240,52 @@ const Dashboard = () => {
         </div>
       </div>
 
-      <div className="flex flex-col md:flex-row my-4">
-      <div className="rounded-3xl shadow mr-3 w-12/12 md:w-4/12 p-2">
-          {!loading ?
-            <Chart
-              chartType="PieChart"
-              width="100%"
-              height="100%"
-              data={!loading ? dataPie : data}
-              options={options}
-            /> : null}
-        </div>
-        <div className="rounded-3xl shadow p-4  mr-0 md:mr-3 w-12/12 md:w-8/12 right-0">
-        <div className="py-2">
+      <div className="flex flex-col lg:flex-row my-4">
+
+        <div className="rounded-3xl shadow p-4  mr-0 lg:mr-3 w-12/12 lg:w-6/12 right-0">
+          <div className="py-2">
             <h1 className="text-lg font-bold">Total de productos por cliente</h1>
           </div>
           <Chart chartType="ColumnChart" width="100%" height="400px" data={!loading ? dataProductsByClients : dataCompany} />
+        </div>
+        <div className="rounded-3xl shadow p-4  mr-0 lg:mr-3 w-12/12 lg:w-6/12">
+          <div className="py-2">
+            <h1 className="text-lg font-bold">Productos con más stock</h1>
+          </div>
+          <div className="text-sm h-64 overflow-auto" >
+            <Table hover responsive className="text-sm">
+              <thead>
+                <tr>
+                  {/* <th>#</th> */}
+                  {user.role === 'SUPER_ADMIN' || user.role === 'SUPPORT' ? <th className="uppercase">Compañía</th> : null}
+                  <th className="uppercase">Cliente</th>
+                  <th className="uppercase">Producto</th>
+                  <th className="uppercase text-center">Cantidad</th>
+                  <th className="uppercase text-center">Código</th>
+               
+
+
+                </tr>
+              </thead>
+              <tbody >
+
+                {map(topProductInventory, (product, index) => {
+                  return (
+                    <tr key={product._id} >
+                      {/* <td className="align-middle">{index + 1}</td> */}
+                      {user.role === 'SUPER_ADMIN' || user.role === 'SUPPORT' ? <td className="align-middle">{product.company.name}</td> : null}
+                      <td className="align-middle">{product.client.name}</td>
+                      <td className="align-middle">{product.name}</td>
+                      <td className="align-middle text-center">{product.cnt}</td>
+                      <td className="align-middle text-center">{product.code}</td>
+                    </tr>
+                  )
+                })}
+
+              </tbody>
+            </Table>
+          </div>
+
         </div>
       </div>
     </div>
